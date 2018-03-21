0baec651d13147b8cbccbba6485e154256883408
Zac Cericola <zcericola@gmail.com>
2018-03-21 16:56:37 -0500

    worked on heading and color scheme

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,7 +9,10 @@ class Home extends Component {
     render(){
         return(<div className = 'home-container'>
         <Header/>
-        <h1>Welcome to the Thunda Dome!!!</h1>
+        <section className = 'heading-text'>
+        <h1>Zac Cericola</h1>
+        <h2>Full Stack Web Developer</h2>
+        </section>
         <Footer/>
         </div>);
     }
