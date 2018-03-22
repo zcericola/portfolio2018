1b4926a88b792914dcffc61ddc3edd55ab04c0b0
Zac Cericola <zcericola@gmail.com>
2018-03-22 16:55:56 -0500

    fixed bg and made transparent

---
+++ src/components/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class About extends Component {
+
+    render(){
+        return(<div className = 'about-container'>
+        <h1>About</h1>
+        </div>
+        );
+    }
+};
+
+export default About;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,6 @@ class Home extends Component {
 
     render(){
         return(<div className = 'home-container'>
-        <img src = {require('../../assets/palm.jpg')} />
         <section className = 'heading-text'>
         <h1>Zac Cericola</h1>
         <h2>Full Stack Web Developer</h2>
