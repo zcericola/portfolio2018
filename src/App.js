c6a3011a552af0e8ff121fb8f14ffb68e915e7a7
Zac Cericola <zcericola@gmail.com>
2018-03-21 21:43:19 -0500

    worked on background image

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import Home from './components/Home/Home';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 class App extends Component {
   render() {
-    return (
-      <div className="app">
-      <Home />       
-      </div>
-    );
+    return <div className="app">
+        <Header />
+        <Home />
+        <Footer />
+      </div>;
   }
 }
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,19 +1,14 @@
 import React,{Component} from 'react';
 
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-
-class Home extends Component {
-    
+class Home extends Component {   
 
     render(){
         return(<div className = 'home-container'>
-        <Header/>
+        <img src = {require('../../assets/palm.jpg')} />
         <section className = 'heading-text'>
         <h1>Zac Cericola</h1>
         <h2>Full Stack Web Developer</h2>
-        </section>
-        <Footer/>
+        </section>        
         </div>);
     }
 }
