cad51c4a8d1897dfb3666b81d2d78aecbb1cfd67
Zac Cericola <zcericola@gmail.com>
2018-03-26 16:57:29 -0500

    set up project cards

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,7 +6,7 @@ class About extends Component {
         return(<div className = 'about-container'>
         
         <p className = 'text' id = 'about'>
-        Lately, I've been using React and Redux on the front-end with Express, Node, and PostGreSQL for the server and database. When I need a break from the technical side of things, I use Adobe Illustrator and Photoshop to hone my design skills.
+        Lately, I've been using React and Redux on the front-end with Express, Node, and PostGreSQL for the server and database management. When I need a break from the technical side of things, I use Adobe Illustrator and Photoshop to hone my design skills.
         </p>       
         
                
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,9 +1,32 @@
 import React, {Component} from 'react';
 
 class Projects extends Component {
+    constructor(){
+        super();
+
+
+    this.displayProjects = this.displayProjects.bind(this);
+    }
+
+    displayProjects(){
+        const projects = [{ title: "Artifact Fine Goods", img: "image", desc: "Artifact is a fictional fullstack e-commerce website" }, { title: "name", img: "image", desc: "desc" }, { title: "name", img: "image", desc: "desc" }, { title: "name", img: "image", desc: "desc" }, { title: "name", img: "image", desc: "desc" }, { title: "name", img: "image", desc: "desc" }, { title: "name", img: "image", desc: "desc" }];
+
+        return projects.map((curr, index) => {
+            return(<div className = 'card'>
+            
+            </div>
+            );
+        })
+
+    }
+
     render(){
         return(<div className = 'projects-container'>
-        <h1>/Projects</h1>
+        <h1 className = 'title'>/Projects</h1>
+        <content className = 'cards-container'>
+            {this.displayProjects()}        
+        </content>
+
 
         
         </div>);
