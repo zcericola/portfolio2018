acdb66a62368f937abba9c20c362dff152fc8d18
Zac Cericola <zcericola@gmail.com>
2018-04-01 17:05:10 -0500

    worked on sizing of font

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,12 +4,12 @@ const Contact = (props) => {
     return <div id="contact">
         <h1 className="title">/Contact</h1>
         <div className="contact">
-          <i className="fas fa-envelope" />
+          <i className="fas fa-envelope contact-pic" />
 
           <span className="contact-item">zcericola@gmail.com</span>
         </div>
         <div className="contact">
-          <i className="fab fa-github" />          
+          <i className="fab fa-github contact-pic" />          
             <span className="contact-item">github.com/zcericola</span>
           
         </div>
--- src/components/Tech/Tech.js
+++ src/components/Tech/Tech.js
@@ -5,19 +5,18 @@ import React from 'react';
 const Tech = (props) => {
     return <div id="tech">
         <h1 className="title">/Tech</h1>
-        <content className ='tech-icons'>
+        <section className ='tech-icons'>
         <i className="icon-html5-alt icon"></i>
         <i className="icon-css3-alt icon"></i>
         <i className="icon-sass icon"></i>
         <i className="icon-javascript-alt icon"></i> 
-        <i className="icon-jquery icon"></i>
-        <i className="icon-bootstrap icon"></i>
+        {/* <i className="icon-jquery icon"></i> */}
+        {/* <i className="icon-bootstrap icon"></i> */}
         <i className="icon-reactjs icon"></i>   
         <i className="icon-angular icon"></i> 
         <i className="icon-nodejs icon"></i>
         <i className="icon-postgres icon"></i>
-        <span className = "tag">tech</span>
-        </content>
+        </section>
       </div>;
     
 
