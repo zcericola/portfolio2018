72158ab869bcb5ee9cedfd2c395bbe76b7a67439
Zac Cericola <zcericola@gmail.com>
2018-03-31 22:22:09 -0500

    made about section mobile responsive

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,8 +3,7 @@ import React, {Component} from 'react';
 class About extends Component {
 
     render(){
-        return(<div id = 'about-two'>
-        
+        return(<div id = 'about-two'>        
         <p className = 'text' id = 'about-text'>
         Lately, I've been using React and Redux on the front-end with Express, Node, and PostGreSQL for the server and database management. When I need a break from the technical side of things, I use Adobe Illustrator and Photoshop to hone my design skills.
         </p>       
